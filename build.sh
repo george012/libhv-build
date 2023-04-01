f0d5dadee046fa8a3df0dea37e0671d8134f69cd
TaoTao <george012@163.com>
2023-04-01 21:35:29 +0800

    6

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ProductName=libhv-build
 Build () 
-{   
-    hv_version=`git submodule update --init --remote --quiet && cd libhv && git describe --tags $(git rev-list --tags --max-count=1) && cd ..` \
+{   git submodule update --init --recursive \
+    && hv_version=`git submodule update --init --remote --quiet && cd libhv && git describe --tags $(git rev-list --tags --max-count=1) && cd ..` \
     && git add . \
     && git commit -m "libhv_version=$hv_version" \
     && git push
