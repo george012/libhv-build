86582c4d4e758645c9d4786944565d300a3f7620
WMYeah-NUC <george012@163.com>
2023-05-26 11:41:32 +0800

    libhv_version=v1.3.1

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 ProductName=libhv-build
 Build () {   
-    hv_version=`git submodule update && cd libhv && git describe --tags $(git rev-list --tags --max-count=1) && cd ..` \
-    && echo $hv_version \
+    hv_version=`git submodule update --init --remote --quiet && cd libhv && git describe --tags $(git rev-list --tags --max-count=1) && cd ..` \
     && git add . \
     && git commit -m "libhv_version=$hv_version" \
     && git push
